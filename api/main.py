2fbf35f9b97972a29c2782df37ce9eda0a3124bd
ChinmayBansal010 <chinmay8521@gmail.com>
2026-01-29 21:19:06 +0530

    initial commit

---
+++ api/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI(title="Chinmay's To-Do API")
+
+class Todo(BaseModel):
+    id: int
+    task: str
+    completed: bool = False
+
+db: List[Todo] = []
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Chinmay Bansal's To-Do API."}
+
+@app.post("/todos", response_model=Todo)
+def create_todo(todo: Todo):
+    db.append(todo)
+    return todo
+
+@app.get("/todos", response_model=List[Todo])
+def get_todos():
+    return db
+
+@app.put("/todos/{todo_id}", response_model=Todo)
+def update_todo(todo_id: int, updated_task: Todo):
+    for index, todo in enumerate(db):
+        if todo.id == todo_id:
+            db[index] = updated_task
+            return updated_task
+    raise HTTPException(status_code=404, detail="Task not found")
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    for index, todo in enumerate(db):
+        if todo.id == todo_id:
+            del db[index]
+            return {"message": f"Task {todo_id} deleted successfully"}
+    raise HTTPException(status_code=404, detail="Task not found")
